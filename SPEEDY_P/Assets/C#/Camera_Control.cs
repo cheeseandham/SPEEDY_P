647031f017a01cce8c639e3101e75648a1dd82f8
cheeseandham <softboxgames@gmail.com>
2017-07-30 16:25:25 +0100

    green Setup

---
+++ SPEEDY_P/Assets/C#/Camera_Control.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Control : MonoBehaviour {
+
+    GameObject camFollow;
+
+    GameObject player;
+    Rigidbody2D playerRb;
+
+    public float offSetCamZ;
+
+    public float smoothTime;
+
+    Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+
+        camFollow = GameObject.FindGameObjectWithTag("CameraFollow");
+	}
+
+    private void FixedUpdate()
+    {
+        if (playerRb != null)
+        {
+            camFollow.transform.position = playerRb.transform.position;
+            Vector3 moveToo = camFollow.transform.TransformPoint(new Vector3(0, 0, -offSetCamZ));
+            transform.position = Vector3.SmoothDamp(transform.position, moveToo, ref velocity, smoothTime);
+        }
+        
+    }
+
+    public void Update_Follow(GameObject newPlayer)
+    {
+        player = newPlayer;
+        playerRb = newPlayer.GetComponent<Rigidbody2D>();
+
+    }
+}
---
+++ SPEEDY_P/Assets/C#/Game_Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Controller : MonoBehaviour {
+
+    Camera mainCam;
+    Camera_Control camControl;
+
+    public GameObject playerOri;
+    public GameObject greenOri;
+
+    // Use this for initialization
+    void Start () {
+
+        if (mainCam == null)
+        {
+            mainCam = Camera.main;
+            camControl = mainCam.GetComponent<Camera_Control>();
+        }
+
+        Spawn_Green(Vector3.zero);
+
+    }
+
+    void Spawn_Green(Vector3 greenPos)
+    {
+        GameObject newGreen = (GameObject)Instantiate(greenOri, greenPos, Quaternion.identity);
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
